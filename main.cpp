1ae222e50fb78fb781a56e1a69c11de22a6c5950
aleksjjj11 <aleksjjj11@fex.net>
2020-05-05 13:46:37 +0500

    Different interesting things

--- main.cpp
+++ main.cpp
@@ -8,14 +8,23 @@ using namespace std;
 const string FALSE = "false";
 const string TRUE = "true";
 
-string parsingDigit(char s, const string &dig);
-string digitFalse(char s, const string &dig, bool isBlock);
-string digitTrue(char s, const string &dig, bool isBlock);
-string parsingAssigment(char s);
-string parsingVariable(char s, const string &variable);
+string parsingDigit(char *s, const string &dig);
+string digitFalse(char *s, const string &dig, bool isBlock);
+string digitTrue(char *s, const string &dig, bool isBlock);
+string parsingAssigment(char *s, string &operand);
+string parsingVariable(char *s, string &operand);
+string parsingInversion(char *s, string &operand);
 
 ofstream fileWrite("output.txt");
 ifstream fileRead;
+
+enum Errors {
+    None,
+    IncorrectAssigment,
+    UsingNotDeclaredVariable,
+    IncorrectInversion,
+    IncorrectNameVariable,
+};
 
 enum Conditions {
     Normal,
@@ -33,6 +42,10 @@ public:
     Variable() {
         name = string();
         value  = string();
+    }
+    Variable(string n, string v) {
+        name = n;
+        value  = v;
     }
     Variable& operator =(Variable el) {
         name = el.name;
@@ -82,22 +95,13 @@ public:
     }
 };
 
+TableVariable Indicators;
+
 int main(int argc, char*argv[]) {
     string fileName = argv[1];
+    int amountOperators = 0;
     char symbol;
-    string digit = "";
-    /*Variable element, element2;
-    element.name = "a";
-    element.value ="true";
-    element2.name = "b";
-    element2.value = "true";
-    TableVariable table;
-    table.Add(element);
-    table.Add(element2);
-    cout << table.count << endl << table.Get("a")->value << endl;
-    element.value = "false";
-    table.Add(element);
-    cout << table.count << endl << table.Get("a")->value << endl;*/
+    string operand = "";
     fileRead = ifstream(fileName);
     fileWrite << "Begin parsing." << endl;
     while (!fileRead.eof()) {
@@ -107,6 +111,14 @@ int main(int argc, char*argv[]) {
             case Normal: {
                 if (symbol == '(') {
                     thisCondition = Assigment;
+                    operand += symbol;
+                    string res = parsingAssigment(&symbol, operand);
+                    fileWrite << "Operator " << ++amountOperators << " : " << operand << endl;
+                    operand = "";
+                    thisCondition = Normal;
+                }
+                if (symbol == '~') {
+
                 }
                 break;
             }
@@ -138,71 +150,165 @@ int main(int argc, char*argv[]) {
 
     }
     fileWrite << "End parsing." << endl;
+    fileWrite << Indicators.count << " variables defined:" << endl;
+    for (int i = 0; i < Indicators.count; i++) {
+        fileWrite << Indicators.variables[i].name << " = " << Indicators.variables[i].value << endl;
+    }
     fileWrite.close();
     return 0;
 }
 
-string digitFalse(char s, const string &dig, bool isBlock) {
+string digitFalse(char *s, const string &dig, bool isBlock) {
     string res = dig;
-    if (FALSE.find(res + s) == string::npos || FALSE.find(res + s) > 0)
+    if (FALSE.find(res + *s) == string::npos || FALSE.find(res + *s) > 0)
         isBlock = true;
     if (isBlock) {
-        res += s;
-        while (isalnum(s) && !fileRead.eof()) {
-            fileRead.get(s);
+        res += *s;
+        while (isalpha(*s) && !fileRead.eof()) {
+            fileRead.get(*s);
             if (fileRead.eof()) break;
-            res += s;
+            res += *s;
         }
         cout << "not digit: " << res;
-        return "none";
+        return res;
     }
-    if (FALSE.find(res + s) == 0 && !(FALSE == res + s)) {
+    if (FALSE.find(res + *s) == 0 && !(FALSE == res + *s)) {
         res += s;
-        fileRead.get(s);
+        fileRead.get(*s);
         return digitFalse(s, res, isBlock);
     }
-    if (FALSE.find(res + s) == 0 && FALSE == res + s) {
-        res += s;
+    if (FALSE.find(res + *s) == 0 && FALSE == res + *s) {
+        res += *s;
         cout << "digit: " << res << endl;
         return res;
     }
 
-    return "none";
+    return res;
 }
 
-string digitTrue(char s, const string &dig, bool isBlock) {
+string digitTrue(char *s, const string &dig, bool isBlock) {
     string res = dig;
-    if (TRUE.find(res + s) == string::npos || TRUE.find(res + s) > 0)
+    if (TRUE.find(res + *s) == string::npos || TRUE.find(res + *s) > 0)
         isBlock = true;
-    if (isBlock) {
-        res += s;
-        while (isalnum(s) && !fileRead.eof()) {
-            fileRead.get(s);
-            if (fileRead.eof()) break;
-            res += s;
-        }
-        cout << "not digit: " << res;
-        return "none";
-    }
-    if (TRUE.find(res + s) == 0 && !(TRUE == res + s)) {
-        res += s;
-        fileRead.get(s);
+
+    if (TRUE.find(res + *s) == 0 && !(TRUE == res + *s)) {
+        res += *s;
+        fileRead.get(*s);
         return digitTrue(s, res, isBlock);
     }
-    if (TRUE.find(res + s) == 0 && TRUE == res + s) {
-        res += s;
+    if (TRUE.find(res + *s) == 0 && TRUE == res + *s) {
+        res += *s;
         cout << "digit: " << res << endl;
         return res;
     }
 
-    return "none";
+    return res;
 }
 //Разбор присваивания
-string parsingAssigment(char s) {
+string parsingAssigment(char *s, string &operand) {
     string res = "";
+    string nameVariable = "";
+    string value = "";
+    bool isEndPars = false;
+    fileRead.get(*s);
+    operand += *s;
+    if (isalpha(*s)) {
+        nameVariable = parsingVariable(s, operand);
+        if (nameVariable == TRUE || nameVariable == FALSE || *s != ',') {
+            fileWrite << "Error " << IncorrectAssigment << ": Incorrect form of assigment." << endl << "Abort parsing." << endl;
+            fileWrite.close();
+            fileRead.close();
+            exit(IncorrectAssigment);
+        }
+        fileRead.get(*s);
+        operand += *s;
+        if (isalpha(*s)) {
+            value = parsingVariable(s, operand);
+            if (value == FALSE) {
+                Indicators.Add(*(new Variable(nameVariable, "false")));
+            } else if (value == TRUE) {
+                Indicators.Add(*(new Variable(nameVariable, "true")));
+            } else {
+                Variable* med = Indicators.Get(value);
+                if (med == nullptr) {
+                    fileWrite << "Error " << UsingNotDeclaredVariable << ": Using not declared variable." << endl << "Abort parsing." << endl;
+                    fileWrite.close();
+                    fileRead.close();
+                    exit(UsingNotDeclaredVariable);
+                }
+                Indicators.Add(*(new Variable(nameVariable, med->value)));
+            }
+            if (*s != ')') {
+                fileWrite << "Error " << IncorrectAssigment << ": Incorrect form of assigment." << endl << "Abort parsing." << endl;
+                fileWrite.close();
+                fileRead.close();
+                exit(IncorrectAssigment);
+            }
+            cout << nameVariable << " " << Indicators.Get(nameVariable)->value << endl;
+            res = "(" + nameVariable + "," + value + ")";
+            return res;
+        }
+        if (*s == '~') {
+            string med = parsingInversion(s, operand);
+            if (med == "none") {
+                fileWrite << "Error " << IncorrectInversion << ": Incorrect struct of inversion." << endl << "Abort parsing." << endl;
+                fileWrite.close();
+                fileRead.close();
+                exit(IncorrectInversion);
+            }
+            value = med;
+            Indicators.Add(*(new Variable(nameVariable, value)));
+            res = "(" + nameVariable + "," + value + ")";
+            return res;
+        }
+        return "none";
+    }
 
+    /*while (!fileRead.eof() && !isEndPars) {
+
+    }*/
 }
 //Разбор имени переменной
-string parsingVariable(char s, const string &variable) {
+string parsingVariable(char *s, string &operand) {
+    string res = "";
+    res = *s;
+    while (!fileRead.eof() && isalpha(*s)) {
+        fileRead.get(*s);
+        operand += *s;
+        if (fileRead.eof()) break;
+        if (!isalpha(*s))
+            break;
+        res += *s;
+    }
+    return res;
+}
 
+string parsingInversion(char *s, string &operand) {
+    string value = "";
+    fileRead.get(*s);
+    operand += *s;
+    if (isalpha(*s)) {
+        value = parsingVariable(s, operand);
+        if (value == FALSE) {
+            return TRUE;
+        } else if (value == TRUE) {
+            return FALSE;
+        } else {
+            Variable* med = Indicators.Get(value);
+            if (med == nullptr) {
+                fileWrite << "Error " << UsingNotDeclaredVariable << ": Using not declared variable." << endl << "Abort parsing." << endl;
+                fileWrite.close();
+                fileRead.close();
+                exit(UsingNotDeclaredVariable);
+            }
+            if (med->value == FALSE)
+                return TRUE;
+            else
+                return FALSE;
+        }
+    }
+    if (*s == '(') {
+
+    }
+    return "none";
 }
